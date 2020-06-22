01bbf82ff49d17cb3de2917d1fc5c28c9bcfbdab
Ian Scott <chesnekov@gmail.com>
2020-06-22 14:46:01 -0700

    add methods to journalObject.js to return the number of vowels or consonants in the journal object body
    Co-Authored-By: Emma Hellman <ehh801@icloud.com>

--- src/journalObject.js
+++ src/journalObject.js
@@ -2,3 +2,33 @@ export function JournalEntry(title, body) {
   this.titleEntry = title;
   this.bodyEntry = body; 
 }
+
+JournalEntry.prototype.vowelCount = function () {
+  let vowelCount = 0;
+  const vowelArray = ["a", "e", "i", "o", "u", "y"];
+
+  for (const letter of this.bodyEntry.toLowerCase()) {
+    for(var i=0; i <= vowelArray.length; i++) {
+      if (letter === vowelArray[i]) {
+        vowelCount++;
+        break;
+      } 
+    }
+  }
+  return vowelCount;
+}
+
+JournalEntry.prototype.consonantCount = function () {
+  const consonatArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+  let consonantCount = 0;
+  
+  for(const letter of this.bodyEntry.toLowerCase()) {
+    for(var i=0; i <= consonantArray.length; i++) {
+      if(letter === consonatArray[i]) {
+        consonantCount++;
+        break;
+      }
+    }
+  }
+  return consonantCount;
+}
